30c5c91c5f06d64bd6ec5dbc44c96716a1c21107
isimmons <isimmons33@gmail.com>
2024-02-26 23:49:44 -0500

    extract and reimplement the js enhancement client side with zod

--- src/utils/client/index.ts
+++ src/utils/client/index.ts
@@ -1,15 +1,56 @@
-export const handleClientForm = (form?: HTMLFormElement | null) => {
-  if (!form) return;
+import contactFormSchema from "~/schemas/contactFormSchema";
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+export const handleContactForm = (
+  contactForm?: HTMLFormElement,
+  messageElement?: HTMLParagraphElement
+) => {
+  if (!contactForm || !messageElement) return;
 
-    fetch(form.action, {
-      method: form.method,
-      body: new FormData(form),
-      headers: {
-        accept: "application/json",
-      },
-    }).then((res) => res.json());
+  const updateMessage = (message: string, type: "error" | "success") => {
+    messageElement.classList.add(type);
+    messageElement.textContent = message;
+    setTimeout(() => {
+      messageElement.textContent = "";
+      messageElement.classList.remove(type);
+    }, 3000);
+  };
+
+  contactForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(contactForm);
+    const { name, email } = Object.fromEntries(formData);
+
+    const validation = contactFormSchema.safeParse({ name, email });
+    if (!validation.success) {
+      return updateMessage(validation.error.errors[0].message, "error");
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/contacts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+      });
+
+      if (!res.ok) throw new Error("Response was not OK");
+
+      contactForm.reset();
+
+      return updateMessage(
+        "Thanks for reaching out. I'll be in touch soon.",
+        "success"
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+        return updateMessage(error.message, "error");
+      }
+      return updateMessage(
+        "Something went wrong. Please try again later",
+        "error"
+      );
+    }
   });
 };
